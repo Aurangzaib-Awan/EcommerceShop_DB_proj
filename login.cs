c7ca6ae6d66cdd7cf706ffaa7c8b55eeddbe6139
Aurangzaib Shehzad <124333016+Aurangzaib-Awan@users.noreply.github.com>
2024-11-23 16:21:38 +0500

    Create login.cs

---
+++ login.cs
@@ -0,0 +1,21 @@
+namespace WinFormsApp1
+{
+    public partial class login_page : Form
+    {
+        public login_page()
+        {
+            InitializeComponent();
+        }
+
+        private void loginpage_load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
