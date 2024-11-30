ede04c539eab4040346e03df21876974d51a5dc3
Aurangzaib Shehzad <124333016+Aurangzaib-Awan@users.noreply.github.com>
2024-11-30 18:54:13 +0500

    Create orderTrack.cs

---
+++ orderTrack.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Order_Tracking : Form
+    {
+        public Order_Tracking()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
