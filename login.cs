072e65935ef6bf1b99bdef3b15120b2cd1a62d84
Aurangzaib Shehzad <124333016+Aurangzaib-Awan@users.noreply.github.com>
2024-11-30 18:49:59 +0500

    Update login.cs
    link the register form to login form

--- login.cs
+++ login.cs
@@ -2,10 +2,24 @@ namespace WinFormsApp1
 {
     public partial class login_page : Form
     {
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Register form
+            Register registerForm = new Register();
+
+            // Show the Register form as a new window
+            registerForm.Show();
+
+            //this hides the login form
+            this.Hide();
+        }
         public login_page()
         {
             InitializeComponent();
+            // Add event handler for the Register button
+            button2.Click += new EventHandler(RegisterButton_Click);
         }
+
 
         private void loginpage_load(object sender, EventArgs e)
         {
@@ -16,6 +30,12 @@ namespace WinFormsApp1
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
+
