2d00700384326df4642e058d70c8874a589caf1f
Aurangzaib Shehzad <124333016+Aurangzaib-Awan@users.noreply.github.com>
2024-11-30 18:52:51 +0500

    Create checkout.cs

---
+++ checkout.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class checkout : Form
+    {
+        public checkout()
+        {
+            InitializeComponent();
+        }
+
+        private void checkout_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
